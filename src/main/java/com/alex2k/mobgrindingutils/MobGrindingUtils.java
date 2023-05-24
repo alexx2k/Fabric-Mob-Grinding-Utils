b19f97683330d1e3d9629f41c6075130772ba969
Alex <alex.balsillie@gmail.com>
2023-05-24 18:50:39 +0100

    initial commit

---
+++ src/main/java/com/alex2k/mobgrindingutils/MobGrindingUtils.java
@@ -0,0 +1,15 @@
+package com.alex2k.mobgrindingutils;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class MobGrindingUtils implements ModInitializer {
+	public static final String MOD_ID = "mobgrindingutils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/alex2k/mobgrindingutils/MobGrindingUtilsClient.java
@@ -0,0 +1,10 @@
+package com.alex2k.mobgrindingutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobGrindingUtilsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
